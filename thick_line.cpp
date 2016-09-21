360b9629d42366b3bee980ce55f2e9231ec749ff
taewook5528 <taewook5528@hanmail.net>
2016-09-21 13:43:38 +0900

    Add files via upload

---
+++ thick_line.cpp
@@ -0,0 +1,78 @@
+#include <GLFW/glfw3.h>
+#include <algorithm>
+
+const int width = 640;
+const int height = 480;
+
+float* pixels = new float[width*height * 3];
+
+void drawPixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
+{
+	pixels[(i + width* j) * 3 + 0] = red;
+	pixels[(i + width* j) * 3 + 1] = green;
+	pixels[(i + width* j) * 3 + 2] = blue;
+}
+
+void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+{
+	for (int i = i0; i <= i1; i++)
+	{
+		const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
+
+		drawPixel(i, j, red, green, blue);
+	}
+}
+
+
+void draw()
+{
+	drawLine(10, 10, 600, 300, 0.0f, 0.0f, 1.0f);
+	drawLine(11, 11, 601, 301, 0.0f, 0.0f, 1.0f);
+	drawLine(12, 12, 602, 302, 0.0f, 0.0f, 1.0f);
+}
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		std::fill_n(pixels, width*height * 3, 1.0f);	// white background
+
+		draw();
+		//drawPixel(50, 50, 1.0f, 0.0f, 0.0f);
+		//drawLine(0, 0, 150, 150, 1.0f, 0.0f, 1.0f);
+
+		glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	delete[] pixels;
+
+	glfwTerminate();
+	return 0;
+}
